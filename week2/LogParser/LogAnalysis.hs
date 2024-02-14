9b9d12ffed7bab0d3b8998afef43cae1ac1dcb53
Ihor Chovpan <ihor@localhost.localdomain>
2024-02-14 14:31:21 -0500

    Add comments to hw2

--- week2/LogParser/LogAnalysis.hs
+++ week2/LogParser/LogAnalysis.hs
@@ -3,6 +3,7 @@ module LogAnalysis where
 
 import Log
 
+-- Parses string into Info, Warning, Error or Unknown LogMessage.
 parseMessage :: String -> LogMessage
 parseMessage str = case (words str) of
                    ("I" : stamp : rest) -> LogMessage Info (read stamp) (unwords rest)
@@ -10,13 +11,20 @@ parseMessage str = case (words str) of
                    ("E" : e : stamp : rest) -> LogMessage (Error (read e)) (read stamp) (unwords rest)
                    _ -> Unknown str
 
+-- Parses each line and adds result to the list
 parseLines :: [String] -> [LogMessage]
 parseLines [] = []
 parseLines (line : rest) = (parseMessage line) : (parseLines rest)
 
+-- Parses the whole result
 parse :: String -> [LogMessage]
 parse str = parseLines (lines str)
 
+-- inserts a LogMessage to a MessageTree.
+-- Do nothing if a root has unknown message, or a new message is unknown
+-- Suppoisng there are no duplications in timestamps
+-- If a new stamp is less than a root stamp, go to left
+-- else, go to right
 insert :: LogMessage -> MessageTree -> MessageTree
 insert (Unknown _) tree = tree
 insert _ tree@(Node _ (Unknown _) _) = tree
@@ -24,14 +32,18 @@ insert msg Leaf = Node Leaf msg Leaf
 insert msg@(LogMessage _ newStamp _) (Node left nodeMsg@(LogMessage _ nodeStamp _) right) = case (newStamp < nodeStamp) of
                                                                                             True -> Node (insert msg left) nodeMsg right
                                                                                             False -> Node left nodeMsg (insert msg right)
+
+-- Builds a MessageTree from a list of LogMessage's.
 build :: [LogMessage] -> MessageTree
 build [] = Leaf
 build (x : xs) = insert x (build xs)
 
+-- Sorts MessageTree in order
 inOrder :: MessageTree -> [LogMessage]
 inOrder Leaf = []
 inOrder (Node left msg right) = (inOrder left) ++ (msg : (inOrder right))
 
+-- filters logmessages based on error severity
 filter50 :: [LogMessage] -> [LogMessage]
 filter50 [] = []
 filter50 (x@(LogMessage (Error severity) _ _) : xs) = let filteredXs = filter50 xs in
