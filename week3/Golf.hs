7e3c3ba795efcad337f69fcc86493d26f9857317
Ihor Chovpan <ihor@localhost.localdomain>
2024-02-19 15:38:21 -0500

    Implement histogram

--- week3/Golf.hs
+++ week3/Golf.hs
@@ -1,4 +1,6 @@
 module Golf where
+import Data.List (intercalate)
+import Data.Char (chr)
 
 -- First, we make an array of skip sizes
 -- Then we create an array of indices for each skip
@@ -26,3 +28,31 @@ window3 xs i = (xs!!i, xs!!(i+1), xs!!(i+2))
 -- Checks whether a middle element of triple is a maxima
 maxima :: (Integer, Integer, Integer) -> Bool
 maxima (a, b, c) = (b > a && b > c)
+
+-- maxDigitOccurence+1 characters is the height of histogram
+histogram :: [Int] -> String
+histogram els = (intercalate "\n" (map (histRow els) [h, h-1..(-1)]))
+                  where h = maxDigitOccurence els
+
+-- given a list and a row coordinate, render that row
+-- 0 refers to the bottow row with ======
+histRow :: [Int] -> Int -> String
+histRow els y = map (\x -> (histChar els x y)) [0..9]
+
+-- Given an original list, determines a character at a specific point of a histogram 
+-- _,0 refers to the bottom row with =======
+-- 0,1 refers to left-most character of 2nd row from the bottom
+histChar :: [Int] -> Int -> Int -> Char
+histChar _ x (-1) = "0123456789" !! (x `mod` 10)
+histChar _ _ 0 = '='
+histChar els x y = case ((occurence els x) >= y) of
+                     True -> '*'
+                     False -> ' '
+
+-- returns the maximum number of times some digit is used in the list.
+maxDigitOccurence :: [Int] -> Int
+maxDigitOccurence xs = maximum $ (map (occurence xs) [1..9])
+
+-- returns the number of time sime element of a list occurs in it
+occurence :: [Int] -> Int -> Int
+occurence xs e = (length . filter (\x -> x == e)) xs
